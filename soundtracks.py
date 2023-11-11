9d70b9fa0312783eee203422c71264dc39cae49e
MuGeminorum <20459298+MuGeminorum@users.noreply.github.com>
2023-11-11 22:19:04 +0800

    add song json

--- soundtracks.py
+++ soundtracks.py
@@ -1,5 +1,7 @@
 import os
+import time
 import json
+import random
 import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup
@@ -7,6 +9,7 @@ from utils import *
 
 
 def get_song_info(song_url):
+    time.sleep(random.uniform(0.1, 1.0))
     try:
         response = requests.get(song_url)
         if response.status_code == 200:
@@ -56,27 +59,34 @@ def get_song_info(song_url):
 
 
 def get_songs(page_url=f"{DOMAIN}/wiki/Category:Soundtrack"):
+    time.sleep(random.uniform(0.1, 1.0))
     soundtracks = {}
-    response = requests.get(page_url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        songs_div = soup.find('div', class_='category-page__members')
-        song_as = songs_div.find_all(
-            name='a',
-            class_='category-page__member-link'
-        )
-        for song_a in tqdm(song_as, desc='Updating soundtracks...'):
-            song_name = song_a.get('title').strip()
-            song_url = f"{DOMAIN}{song_a.get('href')}"
-            soundtracks[song_name] = get_song_info(song_url)
+    try:
+        response = requests.get(page_url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            songs_div = soup.find('div', class_='category-page__members')
+            song_as = songs_div.find_all(
+                name='a',
+                class_='category-page__member-link'
+            )
+            for song_a in tqdm(song_as, desc='Updating soundtracks...'):
+                song_name = song_a.get('title').strip()
+                song_url = f"{DOMAIN}{song_a.get('href')}"
+                soundtracks[song_name] = get_song_info(song_url)
 
-        nextpage = soup.find('a', class_='category-page__pagination-next')
-        if nextpage:
-            nextpage_url = nextpage.get('href')
-            soundtracks = merge_dicts(soundtracks, get_songs(nextpage_url))
+            nextpage = soup.find('a', class_='category-page__pagination-next')
+            if nextpage:
+                nextpage_url = nextpage.get('href')
+                soundtracks = merge_dicts(soundtracks, get_songs(nextpage_url))
 
-    else:
-        print(f'Failed to request {page_url} : HTTP {response.status_code}')
+        else:
+            print(
+                f'Failed to request {page_url} : HTTP {response.status_code}'
+            )
+
+    except requests.RequestException as e:
+        print(f'Error making request of {page_url.split("/")[-1]} : {e}')
 
     return soundtracks
 
